384a17b51b19855e139731bfc4457b2691890b25
João Vítor de Siqueira Campos <joaovitorsc@gmail.com>
2025-05-22 15:41:49 -0400

    Fix: remoção dos logs usados para debug, permaneceu apenas logs de erro

--- src/config/prisma.js
+++ src/config/prisma.js
@@ -1,6 +1,6 @@
 const { PrismaClient } = require('@prisma/client');
 const prisma = new PrismaClient({
-    log: ['query', 'info', 'warn', 'error']
+    log: ['error']
 });
 
 module.exports = prisma;
